acd8aff78f48217b1ffb477d44cbf701f84f4fa9
Yassine Hachguer <yassine.hachguer05@gmail.com>
2025-03-17 08:46:36 +0000

    Premier commit

---
+++ src/main/java/com/example/hospital_mvc/HospitalMvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.hospital_mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalMvcApplication.class, args);
+    }
+
+}

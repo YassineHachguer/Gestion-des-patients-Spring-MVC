a450f16b9730e59e3e8277085884967592679aef
Yassine Hachguer <yassine.hachguer05@gmail.com>
2025-03-24 10:13:42 +0000

    JDBC Authentication ajout des utilisateurs

--- src/main/java/com/example/hospital_mvc/HospitalMvcApplication.java
+++ src/main/java/com/example/hospital_mvc/HospitalMvcApplication.java
@@ -7,8 +7,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
 
 import java.util.Date;
@@ -28,6 +30,14 @@ public class HospitalMvcApplication implements CommandLineRunner {
         patientRepository.save(new Patient(null, "Hanane", new Date(), false, 4321));
         patientRepository.save(new Patient(null, "Imane", new Date(), true, 36));
         patientRepository.save(new Patient(null, "Yassine", new Date(), false, 100));
+    }
+    @Bean
+    CommandLineRunner commandLineRunner(JdbcUserDetailsManager jdbcUserDetailsManager){
+        return args -> {
+            jdbcUserDetailsManager.createUser(User.withUsername("user11").password(passwordEncoder().encode("1234")).roles("USER").build());
+            jdbcUserDetailsManager.createUser(User.withUsername("user22").password(passwordEncoder().encode("1234")).roles("USER").build());
+            jdbcUserDetailsManager.createUser(User.withUsername("admin2").password(passwordEncoder().encode("1234")).roles("USER","ADMIN").build());
+        };
     }
 
     @Bean
